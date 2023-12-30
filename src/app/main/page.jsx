37bf80495c8412ce582b25fbfb5565bcff2c5529
Platon61633 <peredreevplaton@gmail.com>
2023-12-30 21:01:43 +0300

    RightB

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -5,22 +5,24 @@ import { useEffect, useState } from "react";
 
 const Main = () => {
     const [Trains, SetTrains] = useState([])
-    const [WT, SetWT] = useState()
+    // const [WT, SetWT] = useState()
+    const [RightB, SetRightB] = useState(false)
 
   useEffect(()=>{
     axios.get('https://evraz-back.vercel.app/api?need=ns')
     .then(e=>SetTrains(e.data))
+    window.addEventListener("contextmenu", function(e) { e.preventDefault(); })
 
-    for (let i = 0; i < Trains.length; i++) {
-      if (Trains[i]==0) {
-        console.log(0);
-      }else{
-      for (let j = 0; j < Trains[i].length; j++) {
-        console.log(Trains[i][j][3]);
-      }
-    }
+    // for (let i = 0; i < Trains.length; i++) {
+    //   if (Trains[i]==0) {
+    //     console.log(0);
+    //   }else{
+    //   for (let j = 0; j < Trains[i].length; j++) {
+    //     console.log(Trains[i][j][3]);
+    //   }
+    // }
       
-    }
+    // }
   }
   ,[])
 
@@ -28,6 +30,22 @@ const Main = () => {
 
   return (
     <div className="App">
+      {RightB?
+        <div className='RightB' onClick={()=>SetRightB(false)}>
+          <p onClick={e=>e.stopPropagation()} style={{top: RightB[2]-24, left: RightB[1]-8}}>
+            
+            <h1>Вагон №{RightB[0][0]}</h1>
+            <hr />
+            <div className="infor">
+              <div><span>Простой на станции</span><span>{RightB[0][8]}</span></div>
+              <div><span>Собственник</span><span>{RightB[0][1]}</span></div>
+              <div><span>Оператор</span><span>???</span></div>
+              <div><span>Тип вагона</span><span>{RightB[0][4]}</span></div>
+              <div><span>Гружённый</span><span>{RightB[0][6]=='POROZHNIE'?<span>Нет</span>:<span>Да</span>}</span></div>
+            </div>
+          </p>
+        </div>
+        :<></>}
       <div className="table">
         <div className="colomuns info">
           <div className="item">j</div>
@@ -65,9 +83,9 @@ const Main = () => {
             e.map(
               el=>{
                 return(
-              <div className="train" >
+              <div className="train" onContextMenu={(e)=>SetRightB([el,e.clientX, e.clientY])}>
                 <span>{el[0]}</span>
-                <img src={'/trains/'+el[1]+'/'+el[6]+'/'+el[3]+'/1.svg'}/>
+                <img src={'/trains/'+el[1]+'/'+el[6]+'/'+el[3]+'/1.svg'} />
               </div>
             )}
             )
